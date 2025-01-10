aea343da9c8d7d6cf61cd0a5c28be057a3870b06
Yoda_Vipul <vipulpathak113@gmail.com>
2025-01-10 22:41:23 +0530

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import Editor from "@/components/Editor";
 import Preview from "@/components/Preview";
 import ReactPreview from "@/components/ReactPreview";
